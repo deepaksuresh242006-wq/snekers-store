4c2e12960bcd508d07a460c87ff8ac6c045ecfba
deepaksuresh242006-wq <deepaksuresh242006@gmail.com>
2026-01-30 11:52:01 +0530

    feat: Add Firebase integration and configuration

---
+++ lib/firebase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyCiB2bMhsn4dG3w-F3hpsUm6tQBcmmgcXY",
+    authDomain: "sneakers-store-e51ea.firebaseapp.com",
+    projectId: "sneakers-store-e51ea",
+    storageBucket: "sneakers-store-e51ea.firebasestorage.app",
+    messagingSenderId: "376648874415",
+    appId: "1:376648874415:web:42e7f4159370a1c243175a"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
